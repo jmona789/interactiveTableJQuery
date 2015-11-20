7508f57a970da97b6bcdf251ac19dc9c9251ac6d
jmona789 <jmona789@gmail.com>
2015-11-20 15:10:16 -0500

    fixed the function for adding contacts

--- js/script.js
+++ js/script.js
@@ -2,17 +2,29 @@ $(document).ready(function() {
   $(document).on("click", ".removeRowBtn", function(){
     $(this).parent().parent().remove();
   });
-  $(document).on("click", "#addRowBtn", function(){
+  $(document).on("click", "#addRowBtn", function(e){
+    e.preventDefault();
     var newContactFirstName = $("#newRowFirstName").val();
     var newContactLastName = $("#newRowLastName").val();
     var newEmail = $("#newRowEmail").val();
     var deleteBtn = $("<button>").addClass("btn btn-danger removeRowBtn").append("X");
     var newRow = $("<tr>");
-    var firstNameTd = $("<td>").append(newContactFirstName);
-    var lastNameTd = $("<td>").append(newContactLastName);
-    var emailTd = $("<td>").append(newEmail);
+    var firstNameTd = $("<td>").append(newContactFirstName).attr("class", "firstName");
+    var lastNameTd = $("<td>").append(newContactLastName).attr("class", "lastName");
+    var emailTd = $("<td>").append(newEmail).attr("class", "email");
     var deleteBtnTd = $("<td>").append(deleteBtn);
-    newRow.append(firstNameTd).append(lastNameTd).append(emailTd).append(deleteBtnTd);
-    $("#contactsTable").append(newRow);
+    var inputRow = $("#inputRow");
+      if (newContactFirstName.trim() === $(".firstName") && newContactLastName.trim() === $(".lastName") && newEmail.trim() === $(".newEmail")) {
+        alert("You already have that contact added")
+      }
+      else{
+      newRow.append(firstNameTd).append(lastNameTd).append(emailTd).append(deleteBtnTd);
+      $("#contactsTable").append(newRow);
+      $(inputRow).before(newRow);
+      $("#newRowFirstName").val("").focus();
+      $("#newRowLastName").val("");
+      $("#newRowEmail").val("");
+
+    }
   });
 });
